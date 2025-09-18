911533c7c8b27af5033b1b48a7cd96257e967c12
rsabbah <rpasabb@gmail.com>
2025-09-18 18:51:55 +0200

    wezterm color scheme

---
+++ haoov.lua
@@ -0,0 +1,37 @@
+return {
+	foreground = "#E1E4E8",
+	background = "#24292E",
+
+	cursor_bg = "#C8E1FF",
+	cursor_fg = "#000000",
+	cursor_border = "#79B8FF",
+
+	selection_fg = "#E1E4E8",
+	selection_bg = "#586069",
+
+	scrollbar_thumb = "#b5cbd2",
+	split = "#000000",
+
+	ansi = {
+		"#1F1F28", -- black
+		"#ffab70", -- red
+		"#bef5cb", -- green
+		"#77713f", -- yellow
+		"#c8e1ff", -- blue
+		"#b35b79", -- magenta
+		"#d1bcf9", -- teal
+		"#E1E4E8", -- white
+	},
+
+	brights = {
+		"#1b1f23", -- bright black
+		"#f97583", -- bright red
+		"#85e89d", -- bright green
+		"#836f4a", -- bright yellow
+		"#79b8ff", -- bright blue
+		"#624c83", -- bright magenta
+		"#5e857a", -- bright teal
+		"#ffffff", -- bright with
+	},
+
+}
